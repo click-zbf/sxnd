3b0c7dffb215a8d12e23d292a15ffa696772b39d
medic-z <2963845881@qq.com>
2020-01-09 08:06:32 +0800

    订单

--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -5,65 +5,67 @@
     <el-button type="danger" size="small">批量删除</el-button>
     <!-- /按钮 -->
     <!-- 表格 -->
-    <el-table :data="products">
+    <el-table :data="product">
       <el-table-column prop="id" label="编号"></el-table-column>
-            <el-table-column width="200px" prop="name" label="产品名称"></el-table-column>
-            <el-table-column prop="price" label="价格"></el-table-column>
-            <el-table-column width="200px" prop="description" label="描述"></el-table-column>
-            <el-table-column prop="categoryId" label="所属分类"></el-table-column>
-            <el-table-column width="650px" prop="photo" label="照片"></el-table-column>
-            <el-table-column label="操作" fixed="right">
+      <el-table-column width="200" prop="name" label="名称"></el-table-column>
+      <el-table-column width="200" prop="description" label="描述"></el-table-column>
+      <el-table-column prop="price" label="价格"></el-table-column>
+      <el-table-column prop="categoryId" label="所属产品"></el-table-column> 
+      <el-table-column width="300" prop="photo" label="产品照片">
+        <template slot-scope="scope">
+            <img :src="scope.row.photo" width="200" height="200">
+        </template>
+        </el-table-column> 
+
+      <el-table-column label="操作">
         <template v-slot="slot">
-          <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
-          <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
+          <a href="" @click.prevent="toDeleteHandler(slot.row.id)"><i class="el-icon-delete"></i></a>
+          <a href="" @click.prevent="toUpdateHandler(slot.row)"><i class="el-icon-edit-outline"></i></a>
+          <a href="" @click.prevent=""><i class="el-icon-info"></i></a>
         </template>
       </el-table-column>
     </el-table>
     <!-- /表格结束 -->
     <!-- 分页开始 -->
-    <!-- <el-pagination layout="prev, pager, next" :total="50"></el-pagination> -->
+    <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
     <!-- /分页结束 -->
     <!-- 模态框 -->
     <el-dialog
       title="录入产品信息"
       :visible.sync="visible"
       width="60%">
-       
+        ---{{form}}
       <el-form :model="form" label-width="80px">
-       <el-form-item label="产品名称">
-            <el-input v-model="form.name"></el-input>
-            </el-form-item>
+        <el-form-item label="名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        
+        <el-form-item label="价格">
+          <el-input v-model="form.price"></el-input>
+        </el-form-item>
 
-            <el-form-item label="价格">
-            <el-input  v-model="form.price"></el-input>
-            </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.description"></el-input>
+        </el-form-item>
+        <el-form-item label="所属产品">
+          <el-select v-model="form.categoryId" placeholder="请选择">
+          <el-option v-for="item in options" :key="item.id" :label="item.name" :value="item.id">
+          </el-option>
 
-             <el-form-item label="所属栏目">
-                 <el-select v-model="value" placeholder="请选择">
-                 <el-option       v-for="item in options"       
-                 :key="item.id"       
-                 :label="item.name"       
-                 :value="item.id">     
-                 </el-option>
-                 </el-select>
-            </el-form-item>
-
-             <el-form-item label="描述">
-            <el-input  type="textarea" v-model="form.description"></el-input>
-            </el-form-item>
-             <el-form-item label="图片">
-              <el-upload
+          
+        </el-select>
+        </el-form-item>
+        <el-form-item label="图片">
+          <el-upload
               class="upload-demo"
-              action="http://134.175.154.93:6677/file/upload"
-             
-              
+              action="http://134.175.154.93:6677/file/upload/"
               :file-list="fileList"
               :on-success="uploadSuccessHandler"
               list-type="picture">
               <el-button size="small" type="primary">点击上传</el-button>
               <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-            </el-upload>
-            </el-form-item>
+          </el-upload>
+        </el-form-item>
       </el-form>
 
       <span slot="footer" class="dialog-footer">
@@ -76,35 +78,42 @@
   </div>
 </template>
 
+
+
+
 <script>
 import request from '@/utils/request'
 import querystring from 'querystring'
 export default {
   // 用于存放网页中需要调用的方法
   methods:{
+    //上传成功的事件处理函数
     uploadSuccessHandler(response){
-      let photo = "http://134.175.154.93:8888/group1"+response.data.id
-      this.form.photo = photo;
+      let photo = "http://134.175.154.93:8888/group1/"+response.data.id
+      //将图片地址设置到form中，便于一起提交给后台
+      // console.log(response);
+      this.form.photo=photo;
     },
+    //加载栏目信息
     loadCategory(){
-      let url = "http://localhost:6677/category/findAll"
+    let url = "http://localhost:6677/category/findAll"
       request.get(url).then((response)=>{
-        // 将查询结果设置到products中，this指向外部函数的this
-        this.options = response.data;
+        // 将查询结果设置到customers中，this指向外部函数的this
+        this.options= response.data;
       })
     },
     loadData(){
       let url = "http://localhost:6677/product/findAll"
       request.get(url).then((response)=>{
-        // 将查询结果设置到products中，this指向外部函数的this
-        this.products = response.data;
+        // 将查询结果设置到customers中，this指向外部函数的this
+        this.product = response.data;
       })
     },
     submitHandler(){
-      //this.form 对象 ---字符串--> 后台 {type:'product',age:12}
-      // json字符串 '{"type":"product","age":12}'
+      //this.form 对象 ---字符串--> 后台 {type:'customer',age:12}
+      // json字符串 '{"type":"customer","age":12}'
       // request.post(url,this.form)
-      // 查询字符串 type=product&age=12
+      // 查询字符串 type=customer&age=12
       // 通过request与后台进行交互，并且要携带参数
       let url = "http://localhost:6677/product/saveOrUpdate";
       request({
@@ -133,23 +142,20 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        let url="http://localhost:6677/product/deleteById?id="+id;
-      request.get(url).then((response)=>{
-        // 刷新数据
-        this.loadData();
-        // 提示结果
-         this.$message({
+        let url ="http://localhost:6677/product/deleteById?id="+id;
+        request.get(url).then((response)=>{
+          //刷新数据
+          this.loadData();    
+            this.$message({
           type: 'success',
           message: response.message
         });
+        })
       })
-      }
-      );
-       
       
     },
     toUpdateHandler(row){
-      this.fileList = [];
+      this.fileList=[]
       this.form=row;
       this.visible = true;
     },
@@ -157,23 +163,20 @@ export default {
       this.visible = false;
     },
     toAddHandler(){
-      this.fileList = [];
-      this.form={
-
-      }
+      this.form={};
+      this.fileList=[];
       this.visible = true;
     }
   },
+  
   // 用于存放要向网页中显示的数据
   data(){
     return {
       visible:false,
-      fileList:{},
-      products:[],
-      options:[],
-      
-      form:{}
-     
+      product:[],
+      form:{},
+      options:{},
+      fileList:[]
     }
   },
   created(){
